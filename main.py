ca661e5f72b4af24e17ec87fff94450e71643f50
tankh99 <khanghou99@gmail.com>
2025-06-21 20:10:00 +0800

    remove reload=True

--- main.py
+++ main.py
@@ -151,4 +151,4 @@ async def classify_outfit(file: UploadFile = File(...), top_n: int = 5):
 if __name__ == "__main__":
     
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
