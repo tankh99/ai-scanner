a937a37b8bccdd9a715ef954bd0bc9165c65e6e5
tankh99 <khanghou99@gmail.com>
2025-06-21 10:55:23 +0800

    update port

--- main.py
+++ main.py
@@ -129,4 +129,6 @@ async def classify_outfit(file: UploadFile = File(...), top_n: int = 5):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    
+    port = os.environ.get("PORT", 4000)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
